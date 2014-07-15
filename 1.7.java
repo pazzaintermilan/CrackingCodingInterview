f237b0470f4a30f9688194c9ca2d7af6c5c702dd
Jack <forzainterpazza@gmail.com>
2014-07-15 19:59:18 +0800

    Create 1.7.java
    Given an M*N matrix. If an element is 0, set its row and column to 0.

---
+++ 1.7.java
@@ -0,0 +1,20 @@
+    public void solution(int[][] matrix){
+        boolean[] row=new boolean[matrix.length];
+        boolean[] col=new boolean[matrix[0].length];
+        for(int i=0;i<row.length;i++){
+            for(int j=0;j<col.length;j++){
+                if(matrix[i][j]==0){
+                    row[i]=true;
+                    col[j]=true;
+                }
+            }
+        }
+        
+        for(int i=0;i<row.length;i++){
+            for(int j=0;j<col.length;j++){
+                if(row[i]||col[j]){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }

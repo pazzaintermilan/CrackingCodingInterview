4f9c4c1d4da998bd7dc0b774c8d67f2ba08f315f
Jack <forzainterpazza@gmail.com>
2014-07-18 14:20:43 +0800

    Create 2.2.java

---
+++ 2.2.java
@@ -0,0 +1,18 @@
+ public LinkedListNode solution(LinkedListNode head, int k) {
+        if(k<=0)
+            return null;
+        LinkedListNode p1 = head;
+        LinkedListNode p2 = head;
+        for(int i=0;i<k-1;i++){
+            if(p2.next==null)
+                return null;
+            p2=p2.next;
+        }
+        if(p2.next==null)
+            return null;
+        while(p2.next!=null){
+            p2=p2.next;
+            p1=p1.next;
+        }
+        return p1;
+    }

d2909e88004a07e99dd8f2960eb7fc1b5f2c9a30
Jack <forzainterpazza@gmail.com>
2014-07-18 14:23:34 +0800

    Create 2.3.java

---
+++ 2.3.java
@@ -0,0 +1,7 @@
+ public boolean solution(LinkedListNode n){
+        if(n==null||n.next==null)
+            return false;
+        n.data=n.next.data;
+        n.next=n.next.next;
+        return true;
+    }
